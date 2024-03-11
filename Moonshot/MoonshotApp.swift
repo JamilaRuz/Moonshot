af1c8b32ce24b91ecafc4369ee0cb54d26cf7abd
Jamila <j.ruzimetova@gmail.com>
2024-03-11 16:43:06 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jamila Ruzimetova on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
